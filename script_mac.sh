4e9b5bcd3e82e05d9a3fa4e28d3b02bcf116651f
Marco Andrade <marco.andrade@ifro.edu.br>
2022-06-20 13:42:03 -0400

    Update script_mac.sh

--- script_mac.sh
+++ script_mac.sh
@@ -1,1 +1,20 @@
-#/bin/bash
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> /Users/emilia/.zprofile
+
+eval "$(/opt/homebrew/bin/brew shellenv)"
+
+brew install git
+brew install nvm
+
+export NVM_DIR="$HOME/.nvm"
+ 
+[ -s "/opt/homebrew/opt/nvm/nvm.sh" ] && \. "/opt/homebrew/opt/nvm/nvm.sh"
+ 
+[ -s "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm" ] && \. "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm"
+
+nvm install v16.15.1
+
+brew install docker-compose
+
+brew install --cask visual-studio-code
